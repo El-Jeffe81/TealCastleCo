85dac2d41ae9e980a4db1693af75ce6cc2e9be9d
El-Jeffe81 <76759332+El-Jeffe81@users.noreply.github.com>
2026-08-02 17:06:25 -0400

    Implement version tracking from JSON data
    Fetch version information from JSON and display it on the webpage.

--- js/version.js
+++ js/version.js
@@ -1,1 +1,52 @@
+/*
+Teal Castle Co
+Version Tracker
+*/
 
+
+fetch("data/version.json")
+
+.then(response => response.json())
+
+.then(version => {
+
+
+    document.getElementById("version").innerHTML =
+
+    `
+    Website Version: v${version.version}
+    <br>
+    Last Updated: ${version.releaseDate}
+    `;
+
+
+
+    /*
+    Hidden developer information.
+    Can be enabled later during troubleshooting.
+    */
+
+
+    console.log(
+        "Teal Castle Co Build Information",
+        version
+    );
+
+
+})
+
+.catch(error => {
+
+
+    document.getElementById("version").innerHTML =
+
+    "Version unavailable";
+
+
+    console.error(
+        "Version loading error:",
+        error
+    );
+
+
+});
