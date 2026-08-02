1873cdc616d13b7722416a99d4fb8b9790341718
El-Jeffe81 <76759332+El-Jeffe81@users.noreply.github.com>
2026-08-02 17:06:42 -0400

    Implement music toggle button functionality
    Add music controller functionality to toggle audio play and pause.

--- js/music.js
+++ js/music.js
@@ -1,1 +1,46 @@
+/*
+Teal Castle Co
+Music Controller
+*/
 
+
+const audio =
+document.getElementById("themeAudio");
+
+
+const musicButton =
+document.getElementById("musicToggle");
+
+
+
+musicButton.addEventListener(
+"click",
+function(){
+
+
+    if(audio.paused){
+
+
+        audio.play();
+
+
+        musicButton.innerHTML =
+        "🔇 Mute";
+
+
+    }
+
+    else{
+
+
+        audio.pause();
+
+
+        musicButton.innerHTML =
+        "🔊 Music";
+
+
+    }
+
+
+});
